d26d91ba691c67a630e6d96ff037529225aba1f6
Noura Zekry <48691061+nourazekry@users.noreply.github.com>
2023-08-21 22:27:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-script
+
+/* Create a cache object */
+var cache = new LastFMCache();
+
+/* Create a LastFM object */
+var lastfm = new LastFM({
+  apiKey    : 'f21088bf9097b49ad4e7f487abab981e',
+  apiSecret : '7ccaec2093e33cded282ec7bc81c6fca',
+  cache     : cache
+});
+
+/* Load some artist info. */
+lastfm.artist.getInfo({artist: 'The xx'}, {success: function(data){
+  /* Use data. */
+}, error: function(code, message){
+  /* Show error message. */
+}});
